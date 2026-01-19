9e3ca52cea5b062b3ca719591141e3f439bf4f39
duccuong522005 <cuongducmai123@gmail.com>
2026-01-19 17:03:59 +0700

    Add ShowLog script

---
+++ Assets/Scripts/ShowLog.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ShowLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+}
+

a9cd4f4a3bc1dcce55cc876a6d877eef655e3b17
duccuong522005 <cuongducmai123@gmail.com>
2026-01-22 16:29:26 +0700

    Fix PlayerShooting: add instance and weaponPower fields

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public int weaponPower = 1;
+    public int maxweaponPower = 3;
+
+    public static PlayerShooting instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
 
     void Update()
     {
